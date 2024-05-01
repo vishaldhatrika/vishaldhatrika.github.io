fec0fceec59c8ad3c80cfe752148669a3c524a5c
Vishal Dhatrika <dvishal35@gmail.com>
2024-05-01 13:29:16 -0500

    blog post blogPostFetcher.js

---
+++ assets/scripts/blogPostFetcher.js
@@ -0,0 +1,47 @@
+
+const apiKey = 'AIzaSyB-15B1I83PDmCMIlI7cw-074T528fLO14';
+const blogId = '1134659980298051767';
+const maxCharacters = 350; 
+
+fetch(`https://www.googleapis.com/blogger/v3/blogs/${blogId}/posts?key=${apiKey}`)
+.then(response => response.json())
+.then(data => {
+      const postsContainer = document.getElementById('posts-container');
+      let i=0;
+      data.items.forEach(post => {
+            i++;
+            if(i>2){
+                  return;
+            }
+      const postElement = document.createElement('div');
+      postElement.classList.add('post');
+      postElement.classList.add('blog-post-item');
+      
+      postElement.innerHTML = `
+            <h6 style="font-weight:700;">${post.title}</h6>
+            <p> 
+                  ${truncateString(stripHtmlTags(post.content), maxCharacters)}
+                  <br>
+                  <a href="${post.url}" target="_blank" rel="noopener noreferrer">Read more</a>
+                  </p>
+            
+      `;
+      postsContainer.appendChild(postElement);
+      });
+      })
+.catch(error => console.error('Error fetching posts:', error));
+
+
+function truncateString(str, maxLength) {
+      if (str.length > maxLength) {
+      return str.substring(0, maxLength) + '...';
+      } else {
+      return str;
+      }
+}
+
+// Function to strip HTML tags from a string
+function stripHtmlTags(html) {
+      const doc = new DOMParser().parseFromString(html, 'text/html');
+      return doc.body.textContent || "";
+}

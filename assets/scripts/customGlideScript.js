c5ff5ee0d604a11848ba9049ccf33e33fc7b08f7
Vishal Dhatrika <dvishal35@gmail.com>
2024-07-29 02:35:43 -0500

    pause play skill slides

--- assets/scripts/customGlideScript.js
+++ assets/scripts/customGlideScript.js
@@ -1,8 +1,11 @@
-new Glide('#glideTechTools',{
+var tt_playing=false;
+var cp_playing=false;
+
+var techToolsGlide = new Glide('#glideTechTools',{
       type: 'carousel',
       animationTimingFunc: 'linear',
       animateDuration: 1500,
-      hoverpause: true,
+      hoverpause: false,
       autoplay: 1500,
       perView: 7,
       peek: {
@@ -30,15 +33,14 @@ new Glide('#glideTechTools',{
             }
       }
 }).mount();
+tt_playing=true;
 
 
-
-
-new Glide('#glideCodeProfiles',{
+var codeProfGlide = new Glide('#glideCodeProfiles',{
       type: 'carousel',
       animationTimingFunc: 'linear',
       animateDuration: 1500,
-      hoverpause: true,
+      hoverpause: false,
       autoplay: 1500,
       perView: 7,
       peek: {
@@ -66,3 +68,36 @@ new Glide('#glideCodeProfiles',{
             }
       }
 }).mount();
+cp_playing=true;
+
+var cpBtn = document.querySelector("#toggleCodingProfAutoplay").addEventListener("click",function (e) {
+      var complete=false;
+      if( !complete && cp_playing==true){
+            cp_playing=false;
+            codeProfGlide.pause();
+            e.target.innerText = 'play_circle';
+            complete=true;
+      }
+      if(!complete && cp_playing==false){
+            cp_playing=true;
+            codeProfGlide.play();
+            e.target.innerText = 'pause_circle';
+            complete=true;
+      }
+});
+
+var ttBtn = document.querySelector("#toggleTechToolsAutoplay").addEventListener("click",function (e) {
+      var complete=false;
+      if( !complete && tt_playing==true){
+            tt_playing=false;
+            techToolsGlide.pause();
+            e.target.innerText = 'play_circle';
+            complete=true;
+      }
+      if(!complete && tt_playing==false){
+            tt_playing=true;
+            techToolsGlide.play();
+            e.target.innerText = 'pause_circle';
+            complete=true;
+      }
+});
